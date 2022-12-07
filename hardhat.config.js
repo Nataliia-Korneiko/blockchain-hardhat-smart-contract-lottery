9ac1f418a1300c4139bbe8482a988bc297fcc04d
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2022-12-07 13:25:50 +0200

    add deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,35 @@ require('hardhat-contract-sizer');
 require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const {
+  GOERLI_RPC_URL = '',
+  PRIVATE_KEY = '',
+  ETHERSCAN_API_KEY = '',
+  COINMARKETCAP_API_KEY = '',
+} = process.env;
+
 module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
   solidity: '0.8.17',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
